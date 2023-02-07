974006c0ba1aa5cabe00ead7987d4d24b6de5302
SUHAIL-K <contact.canopus@gmail.com>
2023-02-07 20:23:12 +0530

    Initial commit Blog Folder and comps

---
+++ myfitnesspal/src/Components/Blog/BlogPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BlogHeader from './BlogHeader'
+import BlogNavbar from './BlogNavbar'
+
+export default function BlogPage() {
+  return (
+    <div>
+
+        <BlogHeader></BlogHeader>
+        <BlogNavbar></BlogNavbar>
+    </div>
+  )
+}
