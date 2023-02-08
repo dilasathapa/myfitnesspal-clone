eaa6025e61fc6b62afe7d1b7d8ac56ccacb0ff78
Dilasa Thapa <thapadilasa71@gmail.com>
2023-02-08 15:53:40 +0530

    profile started

--- myfitnesspal/src/Components/Routes/AllRoutes.jsx
+++ myfitnesspal/src/Components/Routes/AllRoutes.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../Homepage/Home";
 import Landing from "../Landing/Landing";
+import Profile from "../Homepage/Profile";
 
-const AllRoutes = () =>{
+const AllRoutes = () => {
     return (
         <div>
             <Routes>
                 <Route path='/home' element={<Home />}></Route>
                 <Route path='/' element={<Landing />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
             </Routes>
         </div>
-    ) 
+    )
 }
 
 export default AllRoutes;
