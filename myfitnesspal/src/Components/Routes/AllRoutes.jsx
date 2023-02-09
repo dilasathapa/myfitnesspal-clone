ee9fbe0615cdf4c754662584c29d2d2abb5fb866
Dilasa Thapa <thapadilasa71@gmail.com>
2023-02-09 12:21:01 +0530

    navbar making

--- myfitnesspal/src/Components/Routes/AllRoutes.jsx
+++ myfitnesspal/src/Components/Routes/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Homepage/Home";
 import Landing from "../Landing/Landing";
 import Profile from "../Homepage/Profile";
+import { NavbarChange } from "./Navbar-real";
 
 const AllRoutes = () => {
     return (
@@ -11,6 +12,7 @@ const AllRoutes = () => {
                 <Route path='/home' element={<Home />}></Route>
                 <Route path='/' element={<Landing />}></Route>
                 <Route path="/profile" element={<Profile />}></Route>
+                <Route path="/changednavbar" element={<NavbarChange />}></Route>
             </Routes>
         </div>
     )
